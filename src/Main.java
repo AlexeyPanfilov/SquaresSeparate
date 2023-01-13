698512843b51d0c2ef648417a25dac5c37bca10c
Alexey Panfilov <ap83@list.ru>
2023-01-13 21:26:58 +0300

    Texts changed

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,13 @@ public class Main {
         Instant start = Instant.now();
         for (int i = width; i > 1; i--) {
             if ((width % i == 0) && (length % i == 0)) {
-                System.out.println("сторона квадрата = " + i);
+                System.out.println("сторона квадрата пошаговым способом = " + i);
                 break;
             }
         }
         Instant finish = Instant.now();
         long elapsed = Duration.between(start, finish).toMillis();
-        System.out.println("Время выполнения: " + elapsed);
+        System.out.println("Время выполнения пошагово: " + elapsed);
 
         int newWidth = width;
         //Instant start2 = Instant.now();
@@ -30,11 +30,11 @@ public class Main {
         }
         Instant finish2 = Instant.now();
         long elapsedTotal = Duration.between(start, finish2).toMillis();
-        System.out.println("общее время выполнения: " + elapsedTotal);
-        System.out.println("Рекурсивный метод: " + recursiveSquare(length, width));
+        System.out.println("общее время выполнения программы: " + elapsedTotal);
+        System.out.println("Сторона квадрата рекурсивным методом: " + recursiveSquare(length, width));
         Instant finish3 = Instant.now();
         long elapsedRecursive = Duration.between(start, finish3).toMillis();
-        System.out.println("Время после рекурсии: " + elapsedRecursive);
+        System.out.println("Общее время выполнения программы: " + elapsedRecursive);
     }
 
     static int recursiveSquare (int length, int width) {
